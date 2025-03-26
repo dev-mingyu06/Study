5086f458f78d443e456e44b1abc1c0d63407f279
mingyu06 <chlalsrb00012@gmail.com>
2025-03-26 15:28:52 +0900

    calculator1

--- Calculator.py
+++ Calculator.py
@@ -3,14 +3,14 @@ sum = 0
 
 if menu == 1:
     cal = input("***수식을 입력하세요.:")
-    print(cal+"의 결과는",eval(cal),"입니다다")
+    print(cal+"의 결과는",float(eval(cal)),"입니다다")
 
 elif menu == 2:
-    num1 = int(input("첫 번째 숫자를 입력하세요:"))
-    num2 = int(input("두 번째 숫자를 입력하세요:"))
+    num1 = int(input("***첫 번째 숫자를 입력하세요:"))
+    num2 = int(input("***두 번째 숫자를 입력하세요:"))
     for i in range(num1, num2 + 1):
         sum = sum + i
     print(num1,"+ ... +",num2,"는",sum,"입니다")
     
 else:
-    print("1 또는 2만 입력해주세요요")
+    print("1 또는 2만 입력해주세요")
